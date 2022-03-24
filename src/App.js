df93532d4bd62b05c590ee6b995d3d546d9672b2
Karansxa <karansxa@gmail.com>
2022-03-24 08:12:25 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+const users = [
+  {id:1, name: 'Karan'},
+  {id:2, name: 'Paresh'},
+];
+const Hello = ({users}) => {
+  return(
+    <div>
+      {users.map((user) => (
+        <div>Hello {user.name}</div>
+      ))}
+    </div>
+  )
+}
+
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Karan SHah <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello users={users}/>
+      <button style={{fontSize:30}} onClick={() => {console.log('Hello')}}>Click Me</button>
+      <button style={{fontSize:30}} onClick={() => {console.log('Hello'); console.log('Hello')}}>Click Me</button>
+      <select style={{fontSize: 20}}>
+        <option>Male</option>
+        <option>Female</option>
+      </select>
+      <div>{count}</div>
     </div>
   );
 }
